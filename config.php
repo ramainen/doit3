7afc9d31ff660aef50bfb76d18c9cf1dec2a0a92
ramainen <ainu.sky@gmail.com>
2016-05-05 23:33:52 +0300

    Initial commit. ver 3.0.0.1

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+
+if($_SERVER["SERVER_ADDR"]=='127.0.0.1' || $_SERVER["SERVER_ADDR"]=='192.168.0.1') {
+  define('DB_HOST','127.0.0.1');
+	define('DB_NAME','varvar');
+	define('DB_USER','root');
+	define('DB_PASSWORD','');
+} else {
+	define('DB_HOST','localhost');
+	define('DB_NAME','varvar');
+	define('DB_USER','root');
+	define('DB_PASSWORD','');
+}
+
+//define('MULTISITE',true);
+
+/*
+define('DB_TYPE','pgsql');
+define('DB_HOST','localhost');
+define('DB_NAME','varvar');
+define('DB_USER','postgres');
+define('DB_PASSWORD','12345');
+*/
+
---
+++ core/helpers/helpers.php
@@ -0,0 +1,3 @@
+<?php
+
+//функции https://github.com/laravel/framework/blob/5.2/src/Illuminate/Foundation/helpers.php
